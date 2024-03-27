a417770b6819ec3a790ca39606071db3d280034e
Swathi <155938575+SwaKarthi@users.noreply.github.com>
2024-03-27 11:21:36 +0530

    Update zomato.py

--- zomato.py
+++ zomato.py
@@ -33,7 +33,7 @@ df = df.drop_duplicates()
 
 # Initialize Dash app with suppress_callback_exceptions=True
 app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
-app.server = server
+server = app.server
 # Define main content layout with background image
 app.layout = html.Div([
     html.Div(style={'display': 'flex'}, children=[
